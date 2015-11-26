bfeaf30deaf5b6b0b460b7a6db826ac2f1f856e5
Keidrych Anton-Oates <keidrych@tayloredtechnology.net>
2015-11-26 23:48:31 +1000

    Strip back to only install nightwatch (other npms' aren't needed)

--- install.sh
+++ install.sh
@@ -2,15 +2,9 @@
 
 #base image already has node and nvm installed globally...
 
+
 echo "================= Updating global nodejs packages ==================="
-sudo npm install -g grunt-cli mocha vows phantomjs casperjs;
-
-echo "================ Installing selenium =================="
-sudo mkdir -p /srv;
-cd /srv && sudo wget http://selenium-release.storage.googleapis.com/2.48/selenium-server-standalone-2.48.2.jar;
-
-echo "=============== Installing bower globally ============="
-sudo npm install -g bower
+sudo npm install -g nightwatch;
 
 for file in /tmp/version/*;
 do
